c5f27c314cae7c84a329ed99399f0e315c768a37
Stefania <stefania_stefii97@yahoo.com>
2023-09-18 18:37:20 +0200

    Install Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <h1>Hi</h1>
+      <Button colorScheme="blue">Button</Button>
     </div>
   );
 }
