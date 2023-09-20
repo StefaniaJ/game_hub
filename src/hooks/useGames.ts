1c1848046a1c8610911081e797976705116b99c3
Stefania <stefania_stefii97@yahoo.com>
2023-09-20 19:49:48 +0200

    Displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,7 +12,8 @@ export interface Game {
     id: number;
     name: string;
     background_image: string;
-    parent_platforms: {platform:Platfotm}[]
+    parent_platforms: {platform:Platfotm}[],
+    metacritic: number
   }
   
   interface FetchGamesResponse {
